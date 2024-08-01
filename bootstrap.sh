0b7d855463f62cbf7185f42bf06fb7941291c70d
supu2 <53692719+supu2@users.noreply.github.com>
2024-08-01 22:51:20 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,3 @@ wget -c https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_amd6
 talosctl cluster create --workers 0  --memory 10000 --cpus 8 \
 	--config-patch '[{"op": "add", "path": "/cluster/allowSchedulingOnControlPlanes", "value": true},{"op": "add", "path": "/cluster/extraManifests", "value": ["https://raw.githubusercontent.com/supu2/bootstrap/main/flux.yaml","https://raw.githubusercontent.com/supu2/bootstrap/main/jitsi.yaml","https://raw.githubusercontent.com/supu2/bootstrap/main/ingress-nginx.yaml","https://raw.githubusercontent.com/supu2/bootstrap/main/local-path-provisioner.yaml"]}]' \
 	-p 80:31000/tcp,443:31001/tcp,10000:31002/udp
-./k9s
